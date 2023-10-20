d522b1bf80af775da4b976bd0ad86405efe755fd
Ana Luiza <88158868+Nalu2@users.noreply.github.com>
2023-10-20 14:49:42 -0300

    Create index04.php

---
+++ index04.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>CRUD PHP</title>
+</head>
+<body>
+    <div class="container" style="text-align: center; margin-top: 70px; height: 520px; width: 550px; border: 2px solid black; border-radius: 10px;">
+    <form method="post" action="salvar04.php"> <br>
+        <h1>PRODUTO</h1>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+            Tipo: <input class="form-control" name="tipo_produto">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+            Marca: <input class="form-control" name="marca_produto">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm">
+            Valor: <input class="form-control"  name="valor_produto">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="text-center">
+                <button type="submit" class=" btn btn-primary">Salvar</button>
+            </div>
+        </div>
+
+    </form>
+    </div>
+</body>
+</html>
