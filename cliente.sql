5ad9f6825b4f09cfc7d5f788be3cbedab00cc63e
Ana Luiza <88158868+Nalu2@users.noreply.github.com>
2023-10-22 22:31:01 -0300

    Update cliente.sql

--- cliente.sql
+++ cliente.sql
@@ -38,17 +38,17 @@ CREATE TABLE `cliente` (
 -- Despejando dados para a tabela `cliente`
 --
 
-INSERT INTO `cliente` (`cod_cliente`, `cpf_cliente`, `endereco_cliente`, `nome_cliente`) VALUES
-('472', '12345678900', 'Rua Exemplo, 123', 'Janaina'),
-('39', '98765432101', 'Avenida Principal, 456', 'Alberto'),
-('845', '55555555555', 'Travessa do Centro, 789', 'Carlos'),
-('12', '11122233344', 'Rua da Amostra, 789', 'Daiana'),
-('736', '44433322211', 'Avenida Central, 987', 'Rosa'),
-('198', '99988877766', 'Rua da Simulação, 567', 'Rafaela'),
-('2', '77788899900', 'Avenida Modelo, 345', 'Mario'),
-('3', '22233344455', 'Travessa Abstrata, 123', 'Claudio'),
-('697', '66677788899', 'Rua Genérica, 789', 'Marcia'),
-('42', '33344455566', 'Avenida Imaginária, 234', 'Sandra')
+INSERT INTO `cliente` (`cpf_cliente`, `endereco_cliente`, `nome_cliente`) VALUES
+('12345678900', 'Rua Exemplo, 123', 'Janaina'),
+('98765432101', 'Avenida Principal, 456', 'Alberto'),
+('55555555555', 'Travessa do Centro, 789', 'Carlos'),
+('11122233344', 'Rua da Amostra, 789', 'Daiana'),
+('44433322211', 'Avenida Central, 987', 'Rosa'),
+('99988877766', 'Rua da Simulação, 567', 'Rafaela'),
+('77788899900', 'Avenida Modelo, 345', 'Mario'),
+('22233344455', 'Travessa Abstrata, 123', 'Claudio'),
+('66677788899', 'Rua Genérica, 789', 'Marcia'),
+('33344455566', 'Avenida Imaginária, 234', 'Sandra')
 
 
 --
